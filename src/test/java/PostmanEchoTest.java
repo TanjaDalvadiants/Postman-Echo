4989b97ded8a132ca2c186117e836d6136effc79
tatiana dalvadiants <eparis@atlassian.com>
2023-05-01 08:48:02 +0400

    test passed

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
-class MobileBankApiTestV3 {
+class PostmanEchoTest {
     @Test
     void shouldReturnDemoAccounts() {
         // Given - When - Then
@@ -14,7 +14,7 @@ class MobileBankApiTestV3 {
         given()
                 .baseUri("https://postman-echo.com")
                 .contentType("text/plain; charset=UTF-8")
-                .body("Hello, world!") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .body("Hello,world!") // отправляемые данные (заголовки и query можно выставлять аналогично)
 // Выполняемые действия
                 .when()
                 .post("/post")
