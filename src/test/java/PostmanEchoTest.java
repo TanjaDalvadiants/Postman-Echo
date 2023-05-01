4ad56ca46a1819e37209477978f72a0945b2efb8
tatiana dalvadiants <eparis@atlassian.com>
2023-05-01 08:36:32 +0400

    test changed

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,27 @@
+
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+class MobileBankApiTestV3 {
+    @Test
+    void shouldReturnDemoAccounts() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Hello, world!") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Hello,world!"))
+        ;
+    }
+}
